b15b32a157a7627d2d20bd4916c92d9e0e427645
Mohit Saini <mohitbhatwara2@gmail.com>
2023-07-22 09:49:46 +0530

    Updates

---
+++ precache-manifest.598c42aa6be0ceef1d159f644dab555e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "448ed75ccf12d3f50827",
+    "url": "/CaseBuddy/static/js/main.448ed75c.chunk.js"
+  },
+  {
+    "revision": "c31350e6c4d51234da9c",
+    "url": "/CaseBuddy/static/js/1.c31350e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/CaseBuddy/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "a8a5e9a49b97167f9378",
+    "url": "/CaseBuddy/static/js/runtime~main.a8a5e9a4.js"
+  },
+  {
+    "revision": "988286fea3a8da7f694c4cdfe1587872",
+    "url": "/CaseBuddy/index.html"
+  }
+];
