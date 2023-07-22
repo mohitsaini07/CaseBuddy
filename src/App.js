5c03ab759be72c07fc925360df108110f6c0cb9c
Mohit Saini <mohitbhatwara2@gmail.com>
2023-07-22 09:44:24 +0530

    first time

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+// import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import Alert from "./Components/Alert";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState("light"); //whether darkmode is enables or not
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#343a40";
+      showAlert("dark mode is enabled", "success");
+      document.title = "TextUtils-Dark-Mode";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode is enabled", "success");
+      document.title = "TextUtils-Light-Mode";
+    }
+  };
+
+  return (
+    <>
+      {/* <Router> */}
+      <div className="vh-100">
+        <Navbar
+          title="CaseBuddy"
+          aboutText="About"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        <Alert alert={alert} />
+        {/* <Navbar /> default props */}
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About mode={mode} />}></Route>
+            <Route */}
+        {/* exact path="/" element= */}
+        {/* { */}
+        <div className="container shadow-lg rounded my-4 p-4">
+          <TextForm
+            heading="Change to UPPERCASE/lowercase:"
+            mode={mode}
+            showAlert={showAlert}
+          />
+        </div>
+        {/* } */}
+        {/* ></Route> */}
+        {/* </Routes> */}
+        {/* <About mode={mode} /> */}
+      </div>
+      {/* </Router> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TextForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const changeUppercase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to Uppercase", "success");
+  };
+
+  const changeLowercase = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert("Converted to Lowercase", "success");
+  };
+
+  const clearText = () => {
+    let newText = "";
+    setText(newText);
+    props.showAlert("Text is Cleared", "success");
+  };
+
+  const copyText = () => {
+    let textArea = document.getElementById("myTextarea");
+    textArea.select();
+    navigator.clipboard.writeText(text);
+    props.showAlert("Text is Copied", "success");
+  };
+
+  const removeExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+    props.showAlert("Extra spaces has been Removed", "success");
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+
+  const [text, setText] = useState("");
+  //   text = "Software Developer"; //Wrong way to change the state
+  //   setText("New text"); //Correct way to change the state
+  return (
+    <>
+      <div
+        className="container my-4 "
+        style={{
+          color: props.mode === "light" ? "black" : "white",
+        }}
+      >
+        <div className="fs-5 my-4">{props.heading}</div>
+        <div className="mb-3">
+          <textarea
+            className="form-control device-width"
+            value={text}
+            onChange={handleOnChange}
+            id="myTextarea"
+            rows="10"
+            style={{
+              backgroundColor: props.mode === "dark" ? "#343a40" : "white",
+              color: props.mode === "light" ? "black" : "white",
+            }}
+          ></textarea>
+        </div>
+        <div className="d-flex text-center gap-3">
+          <button className="btn btn-light" onClick={changeUppercase}>
+            Convert to Uppercase
+          </button>
+          <button className="btn btn-light" onClick={changeLowercase}>
+            Convert to Lowercase
+          </button>
+          <button className="btn btn-light" onClick={clearText}>
+            Clear
+          </button>
+          <button className="btn btn-light" onClick={copyText}>
+            Copy
+          </button>
+          <button className="btn btn-light" onClick={removeExtraSpaces}>
+            Remove Extra Spaces
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        <h2 style={{ color: props.mode === "dark" ? "white" : "black" }}>
+          Your Text Summary
+        </h2>
+        <p style={{ color: props.mode === "dark" ? "white" : "black" }}>
+          {text.split(" ").length} Words and {text.length} Characters
+        </p>
+      </div>
+    </>
+  );
+}
